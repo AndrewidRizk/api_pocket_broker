c9c92f4116f8bcd7d23703a69b7a8317348509c0
AndrewidRizk <97995173+AndrewidRizk@users.noreply.github.com>
2025-09-14 10:18:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ llm.py
@@ -0,0 +1,74 @@
+# Import needed libs
+# from langchain_community.tools import DuckDuckGoSearchRun
+# from langchain_community.document_loaders import WebBaseLoader
+# from langchain.tools import Tool
+# from langchain.agents import AgentExecutor, create_tool_calling_agent
+# from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import requests
+import os
+import json
+
+# Initialize Gemini model
+
+from typing import Dict
+from dotenv import load_dotenv
+import requests
+import json
+
+## Load env variables
+load_dotenv()
+
+## Initialize models
+grok_api_url = "https://api.x.ai/v1/chat/completions"
+
+def get_payload(portfolio: Dict):
+    return {
+        "messages": [
+            {
+                "role": "user",
+                "content": f"""
+                # Cryptocurrency Portfolio Analysis and Investment Recommendation
+
+                ## Current Portfolio
+                I currently hold the following cryptocurrency positions:
+
+                {portfolio}
+
+
+                ## Research Request
+                Please conduct thorough market research on current cryptocurrency trends, market conditions, and emerging opportunities. Based on this research and my existing portfolio composition, provide a smart diversification recommendation on what i should buy.
+
+
+                ## Analysis Requirements
+                1. **Market Research**: Analyze current market conditions, trends, and sentiment
+                2. **Portfolio Assessment**: Evaluate my current holdings and identify gaps or overexposure
+                3. **Diversification Strategy**: Recommend a action that would improve portfolio balance
+                4. **Risk Management**: Consider how the recommendation fits my risk profile
+
+                ## Output Format
+                Token: [TOKEN_NAME]
+                Action: [BUY/SELL/HOLD],
+                Price: [PRICE],
+                Quantity: [QUANTITY],
+                Reasoning: [Detailed explanation including market analysis, portfolio fit, risk assessment, and diversification benefits]
+                """
+            }
+            ],
+            "search_parameters": {
+                "mode": "auto",
+                "max_search_results": 10,
+                "from_date": "2025-01-01",
+                "to_date": "2025-12-31"
+            },
+            "model": "grok-4"
+        }
+
+def send_grok_request(payload: Dict):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('XAI_API_KEY')}"
+    }
+
+    response = requests.post(grok_api_url, headers=headers, json=get_payload(payload))
+    return response.json()
---
+++ service.py
@@ -0,0 +1,249 @@
+from collections import defaultdict
+import json
+from typing import Dict, Optional, Tuple, List
+from utils import request
+from data import TICKER_MAPPINGS
+
+'''
+Returns the asset information for the given symbol.
+'a' -> Ask
+'b' -> Bid
+'c' -> Close
+'v' -> Volume
+'''
+
+def retrieve_asset_info(pair: str) -> Tuple[Dict, Dict]:
+    response = request(
+        method="GET", 
+        path="/0/public/Ticker",
+        query={'pair': pair}
+    )
+
+    response_data = response.read().decode('utf-8')
+
+    json_data = json.loads(response_data)
+
+    if response.status != 200 or ('error' in json_data and len(json_data['error'])):
+        return None, json_data['error']
+
+    result = json_data['result']
+    return result, None
+
+def retrieve_asset_pair_name(symbol1: str, symbol2: str) -> Tuple[Dict, Dict]:
+    response = request(
+        method="GET", 
+        path="/0/public/AssetPairs",
+        query={'pair': f'{symbol1}/{symbol2}'}
+    )
+
+    response_data = response.read().decode('utf-8')
+
+    json_data = json.loads(response_data)
+
+    if response.status != 200 or not 'result' in json_data:
+        return None, json_data['error']
+
+    result = json_data['result']
+
+    return result, None
+
+def retrieve_trades_history() -> Tuple[Dict, Dict]:
+    response = request(
+        method="POST", 
+        path="/0/private/TradesHistory"
+    )
+    
+    response_data = response.read().decode('utf-8')
+
+    json_data = json.loads(response_data)
+
+    if response.status != 200:
+        return None, json_data['error']
+
+    result = json_data['result']
+    trades = defaultdict(list)
+
+    for trade in result['trades'].values():
+        if trade['type'] == 'sell':
+            if trade['pair'] in trades:
+                trades[trade['pair']] = []
+            continue
+
+        trades[trade['pair']].append({
+            'pair': trade['pair'],
+            'time': trade['time'],
+            'type': trade['type'],
+            'amount': float(trade['vol']),
+            'price': float(trade['price']),
+            'cost': float(trade['cost']),
+            'fee': float(trade['fee']),
+            'margin': float(trade['margin']),
+        })
+
+    return trades, None
+
+def compute_asset_profit_loss(current: float, trades: List[Dict]) -> float:
+    total = 0
+    for trade in trades:
+        total += trade['cost']
+
+    return current - total
+
+def get_kraken_ticker_pair(symbol: str) -> str:
+    symbol = symbol.upper()
+    
+    for internal_name, mapping_info in TICKER_MAPPINGS.items():
+        if symbol in mapping_info['kraken_ticker']:
+            for pair in mapping_info["kraken_fiat_pairs"]:
+                if "USD" in pair:
+                    return pair
+            
+    return None
+
+def retrieve_portfolio() -> Tuple[Dict, Dict]:
+    response = request(
+        method="POST", 
+        path="/0/private/Balance"
+    )
+    
+    response_data = response.read().decode('utf-8')
+
+    json_data = json.loads(response_data)
+
+    if response.status != 200:
+        return None, json_data['error']
+        
+    result = json_data['result']
+
+    portfolio = []
+
+    trades, error = retrieve_trades_history()
+
+    if error:
+        return None, error
+
+    total_loss_for_all_assets = 0
+
+    assets = {}
+
+    asset_symbols = ""
+    equivalents = {}
+
+    for symbol in result.keys():
+        kraken_ticker_pair = get_kraken_ticker_pair(symbol)
+
+        if kraken_ticker_pair is None:
+            continue
+
+        equivalents[symbol] = kraken_ticker_pair
+        asset_symbols += kraken_ticker_pair + ","
+
+    asset_symbols = asset_symbols[:-1]
+
+    assets_info, error = retrieve_asset_info(asset_symbols.encode())
+
+    if error:
+        return None, error
+
+    total_holdings = float(result['ZUSD'])
+
+    for ticker, pair in equivalents.items():
+        assets[pair] = assets_info[pair]
+        total_holdings += float(assets[pair]['c'][0]) * float(result[ticker])
+
+    for ticker, pair in equivalents.items():
+        history = trades.get(pair)
+
+        if float(result[ticker]) == 0.00:
+            continue
+
+        asset_value = float(result[ticker]) * float(assets[pair]['c'][0])
+        
+        current_loss = compute_asset_profit_loss(asset_value, history) if history else 0
+        total_loss_for_all_assets += current_loss
+
+        price = float(assets[pair]['c'][0])
+
+        asset_data = {
+            'symbol': ticker,
+            'holding_amount': float(result[ticker]),
+            'profit_loss': current_loss,
+            'price': price,
+            'value': asset_value,
+            'weight': asset_value / total_holdings
+        }
+
+        portfolio.append(asset_data)
+
+    portfolio.append({
+        'symbol': 'USD',
+        'holding_amount': float(result['ZUSD']),
+        'profit_loss': 0,
+        'price': 1,
+        'value': float(result['ZUSD']),
+        'weight': float(result['ZUSD']) / total_holdings
+    })
+
+    data = {
+        'positions': portfolio,
+        'total_profit_loss': total_loss_for_all_assets,
+        'total_holdings': total_holdings
+    }
+
+    return data, None
+
+def execute_buy_order(pair: str, amount: float, order_type: str = 'market', price: Optional[float] = None) -> Tuple[Dict, Dict]:
+    body = {
+        'ordertype': order_type,
+        'type': 'buy',
+        'volume': amount,
+        'pair': pair    
+    }
+
+    if order_type == 'limit':
+        if not price:
+            return None, 'Price is required for limit orders'
+
+        body['price'] = price
+
+    response = request(
+        method="POST", 
+        path="/0/private/AddOrder",
+        body=body
+    )
+
+    response_data = response.read().decode('utf-8')
+    json_data = json.loads(response_data)
+
+    if 'error' in json_data and len(json_data['error']):
+        return None, json_data['error']
+
+    return json_data['result'], None
+
+def execute_sell_order(pair: str, amount: float, order_type: str = 'market', price: Optional[float] = None) -> Tuple[Dict, Dict]:
+    body = {
+        'ordertype': order_type,
+        'type': 'sell',
+        'volume': amount,
+        'pair': pair
+    }
+
+    if order_type == 'limit':
+        if not price:
+            return None, 'Price is required for limit orders'
+
+        body['price'] = price
+
+    response = request(
+        method="POST", 
+        path="/0/private/AddOrder",
+        body=body
+    )
+
+    response_data = response.read().decode('utf-8')
+    json_data = json.loads(response_data)
+
+    if 'error' in json_data and len(json_data['error']):
+        return None, json_data['error']
+
+    return json_data['result'], None
